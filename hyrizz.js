6dae6637bc120cf87d05c9cc1192a57ee773d684
tratzy <naswacerih@gmail.com>
2026-03-06 02:06:44 +0700

    Add files via upload

---
+++ hyrizz.js
@@ -0,0 +1,32 @@
+const apks = [
+  {
+    title: "🎟️ hyrizz free",
+    desc: "fiturnya banyak dan melimpah .",
+    note: "Note: bisa lu jual lagi bebas kasih harga asal laku.",
+    link: "https://sfl.gl/fAjS"
+  },
+];
+
+const apkList = document.getElementById("apk-list");
+
+apks.forEach((apk, index) => {
+  const card = document.createElement("div");
+  card.classList.add("apk-card");
+
+  card.innerHTML = `
+    <div class="apk-title">${apk.title}</div>
+    <div class="apk-desc">${apk.desc}</div>
+    <div class="apk-note">${apk.note}</div>
+    <button class="download-btn">DOWNLOAD</button>
+  `;
+
+  card.querySelector(".download-btn").addEventListener("click", () => {
+    window.location.href = apk.link;
+  });
+
+  apkList.appendChild(card);
+
+  setTimeout(() => {
+    card.classList.add("show");
+  }, 200 * index);
+});
